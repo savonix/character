e3ff27a60be790df8992a012c9e69c6bbd9edb31
docunext <albert.lash@docunext.com>
2011-11-12 14:15:10 -0500

    locales, capybara

--- app/models/character/user_profile.rb
+++ app/models/character/user_profile.rb
@@ -1,5 +1,6 @@
 class Character::UserProfile < ActiveRecord::Base
   serialize :custom
   belongs_to :user, :class_name => Character.user_class.to_s
+  #mount_uploader :picture, ImageUploader
 
 end
